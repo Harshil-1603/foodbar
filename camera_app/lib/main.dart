00ae6e48b3c8b8a0e11eb93257046f34f46310c5
Harshil-1603 <harshil.agrawal098@gmail.com>
2025-10-15 09:15:04 +0530

    first commit

---
+++ camera_app/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+List<CameraDescription>? cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const CameraApp());
+}
+
+class CameraApp extends StatefulWidget {
+  const CameraApp({super.key});
+
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  CameraController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+    if (cameras != null && cameras!.isNotEmpty) {
+      controller = CameraController(cameras![0], ResolutionPreset.max);
+      controller?.initialize().then((_) {
+        if (!mounted) {
+          return;
+        }
+        setState(() {});
+      }).catchError((Object e) {
+        if (e is CameraException) {
+          switch (e.code) {
+            case 'CameraAccessDenied':
+              print('User denied camera access.');
+              break;
+            default:
+              print('Handle other errors.');
+              break;
+          }
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller == null || !controller!.value.isInitialized) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return MaterialApp(
+      home: CameraPreview(controller!),
+    );
+  }
+}
